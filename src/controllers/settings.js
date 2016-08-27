93a7648b692cc485f6358122d0b319b554733df3
Ajay Sreedhar <steelcode468@gmail.com>
2016-08-27 20:21:05 +0530

    [added] option to toggle animation

--- src/controllers/settings.js
+++ src/controllers/settings.js
@@ -1,8 +1,9 @@
-app.controller('SettingsController', ['$scope', 'viewFactory', 'toast', function ($scope, viewFactory, toast) {
+app.controller('SettingsController', ['$rootScope', '$scope', 'viewFactory', 'toast', function ($rootScope, $scope, viewFactory, toast) {
     viewFactory.deleteAction = viewFactory.prevUrl = null;
     viewFactory.title = 'Settings';
 
     $scope.kongConfig = kongConfig;
+    $scope.appConfig = ipcRenderer.sendSync('get-app-config', '');
 
     var formKongConfig = angular.element('form#formKongConfig');
 
@@ -16,10 +17,32 @@ app.controller('SettingsController', ['$scope', 'viewFactory', 'toast', function
         ipcRenderer.on('write-kong-config-success', function () {
             toast.success('Kong configuration saved')
 
-        }).on('write-kong-config-error', function (event, arg) {
+        }).on('write-app-config-error', function (event, arg) {
             toast.error(arg.message)
         });
 
         return false;
-    })
+    });
+
+    angular.element('#toggleAnimation').on('click', function (event) {
+        var checkbox = angular.element(event.target);
+
+        if (checkbox.is(':checked')) {
+            $rootScope.ngViewAnimation = 'slide-right';
+            $scope.appConfig.enableAnimation = true;
+
+        } else {
+            $rootScope.ngViewAnimation = '';
+            $scope.appConfig.enableAnimation = false;
+        }
+
+        ipcRenderer.send('write-app-config', $scope.appConfig);
+
+        ipcRenderer.on('write-app-config-success', function () {
+            toast.success('App configuration saved')
+
+        }).on('write-app-config-error', function (event, arg) {
+            toast.error(arg.message)
+        });
+    });
 }]);
