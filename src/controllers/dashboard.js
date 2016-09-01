69fd342b5fb662050e7e4630d91763d2265109f2
Ajay Sreedhar <steelcode468@gmail.com>
2016-09-01 17:29:53 +0530

    [added] ajax and toast dependency

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,15 +1,15 @@
-app.controller('DashboardController', ['$scope', 'kdAjax', 'kdToast', 'viewFactory', function ($scope, kdAjax, kdToast, viewFactory) {
+app.controller('DashboardController', ['$scope', 'ajax', 'toast', 'viewFactory', function ($scope, ajax, toast, viewFactory) {
 
     viewFactory.title = 'Dashboard';
     viewFactory.prevUrl = null;
 
-    kdAjax.get({
+    ajax.get({
         resource: '/'
     }).then(function (response) {
         $scope.kongStat = response.data;
         $scope.database = $scope.kongStat.configuration.database;
 
     }, function () {
-        kdToast.error('Could not populate data');
+        toast.error('Could not populate data');
     })
 }]);
