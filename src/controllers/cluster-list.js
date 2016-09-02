21c6bb90377792c5293d32c34b0144ead170efb2
Ajay Sreedhar <steelcode468@gmail.com>
2016-09-02 18:48:49 +0530

    [added] ClusterListController

---
+++ src/controllers/cluster-list.js
@@ -0,0 +1,27 @@
+(function (app) {
+
+    var controller = "ClusterListController";
+    if (typeof app === 'undefined') throw (controller + ': app is undefined');
+
+    app.controller(controller, ['$scope', 'ajax', 'toast', 'viewFactory', function ($scope, ajax, toast, viewFactory) {
+
+        viewFactory.title = 'Cluster';
+        viewFactory.prevUrl = null;
+
+        $scope.clusterList = [];
+
+        $scope.fetchClusterList = function (resource) {
+            ajax.get({ resource: resource }).then(function (response) {
+                $scope.nextUrl = response.data.next || '';
+
+                for (var i = 0; i < response.data.data.length; i++ ) {
+                    $scope.clusterList.push(response.data.data[i]);
+                }
+            }, function () {
+                toast.error('Could not load list of clusters');
+            });
+        };
+
+        $scope.fetchClusterList('/cluster');
+    }]);
+})(app);
