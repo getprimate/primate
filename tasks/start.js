f70243e97fd67e1398706940346f3b4f95f7871c
Ajay Sreedhar <steelcode468@gmail.com>
2016-09-04 20:44:53 +0530

    [moved] start task to start.js

---
+++ tasks/start.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function (gulp) {
+    var childProcess = require('child_process');
+    var electron = require('electron-prebuilt');
+
+    gulp.task('start', (next) => {
+        var child = childProcess.spawn(electron, ['./']);
+
+        child.stdout.on('data', (data) => {
+            console.log(data.toString('utf-8'));
+        });
+
+        child.on('exit', (code) => {
+            console.log('Child exited with code: ' + code);
+            return next(code === 1 ? new Error('Error running run task') : null);
+        });
+    });
+};
