e64dcb5cbf89e42eb1d93f4cdba2d3264d9f15fa
Ajay Sreedhar <steelcode468@gmail.com>
2016-09-05 14:32:54 +0530

    [added] pack script for mac osx

--- tasks/pack.js
+++ tasks/pack.js
@@ -70,7 +70,7 @@ module.exports = function (gulp, packageJson) {
     gulp.task('pack-osx', (next) => {
         electronPackager(_.extend(RELEASE_SETTINGS, {
             platform: 'mas',
-            arch: 'all',
+            arch: 'x64',
             icon: 'resources/icons/kongdash-256x256.icns'
         }), next);
     });
