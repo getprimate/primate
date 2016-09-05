b5336719bae3cfb725bb24885bb3c4b88859c06a
Ajay Sreedhar <steelcode468@gmail.com>
2016-09-05 20:44:27 +0530

    [changed] mac platform name to darwin

--- tasks/pack.js
+++ tasks/pack.js
@@ -69,7 +69,7 @@ module.exports = function (gulp, packageJson) {
 
     gulp.task('pack-osx', (next) => {
         electronPackager(_.extend(RELEASE_SETTINGS, {
-            platform: 'mas',
+            platform: 'darwin',
             arch: 'x64',
             icon: 'resources/icons/kongdash-256x256.icns'
         }), next);
