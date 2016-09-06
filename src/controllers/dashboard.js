e8f55b1b89a3e1e9da553b3eeb8a7be08b3df749
Ajay Sreedhar <steelcode468@gmail.com>
2016-09-06 19:48:24 +0530

    [BUGFIX] function createChart modified

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -11,7 +11,7 @@
     var createChart = function (container, data) {
         if (typeof data.options === 'undefined') data.options = {responsive: true, scales: {xAxes: [{ticks: {beginAtZero: true}}]}};
 
-        return new Chart (angular.element(container).getContext('2d'), data);
+        return new Chart (angular.element(container)[0].getContext('2d'), data);
     };
 
     app.controller(controller, ['$scope', 'ajax', 'toast', 'viewFactory', function ($scope, ajax, toast, viewFactory) {
@@ -38,15 +38,6 @@
         ajax.get({ resource: '/status' }).then(function (response) {
             var server = response.data.server;
 
-            var chartConfig = {
-                labels: ['Handled', 'Accepted', 'Active', 'Waiting', 'Reading', 'Writing'],
-                data: [server.connections_handled, server.connections_accepted, server.connections_active,
-                    server.connections_waiting, server.connections_reading, server.connections_writing],
-                backgrounds: ['rgba(24, 138, 226, 0.5)', 'rgba(16, 196, 105, 0.5)', 'rgba(128, 197, 218, 0.5)',
-                    'rgba(248, 142, 15, 0.5)', 'rgba(207, 32, 241, 0.5)', 'rgba(91, 105, 188, 0.5)'],
-                borders: ['#188AE2', '#10C469', '#80C5DA', '#F88E0F', '#CF20F1', '#5B69BC']
-            };
-
             createChart('#clusterStatChart', {
                 type: 'bar',
                 data: { labels: ['Handled', 'Accepted', 'Active', 'Waiting', 'Reading', 'Writing'], datasets: [{
@@ -55,7 +46,7 @@
                     backgroundColor: ['rgba(24, 138, 226, 0.5)', 'rgba(16, 196, 105, 0.5)', 'rgba(128, 197, 218, 0.5)',
                         'rgba(248, 142, 15, 0.5)', 'rgba(207, 32, 241, 0.5)', 'rgba(91, 105, 188, 0.5)'],
                     borderColor: ['#188AE2', '#10C469', '#80C5DA', '#F88E0F', '#CF20F1', '#5B69BC'],
-                    borderWidth: 1
+                    borderWidth: 1, label: 'Connections'
                 }] }
             });
 
