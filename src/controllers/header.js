91f67c706f1220b46c369e949765e1593f8ff6fe
Ajay Sreedhar <steelcode468@gmail.com>
2017-07-29 21:10:51 +0530

    change: use const to declare controller name

--- src/controllers/header.js
+++ src/controllers/header.js
@@ -1,13 +1,10 @@
 /* global app:true */
 (function (app) { 'use strict';
-
-    var controller = 'HeaderController';
-
+    const controller = 'HeaderController';
     if (typeof app === 'undefined') throw (controller + ': app is undefined');
 
     app.controller(controller, ['$scope', 'viewFactory', function ($scope, viewFactory) {
         $scope.viewFactory = viewFactory;
         $scope.title = 'Dashboard';
     }]);
-
 })(app);
