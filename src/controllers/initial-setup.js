069a02af9b3b3b5bdbabf331a47078e072f58da2
Ajay Sreedhar <steelcode468@gmail.com>
2017-07-29 21:29:23 +0530

    change: use const to declare controller name

--- src/controllers/initial-setup.js
+++ src/controllers/initial-setup.js
@@ -1,32 +1,30 @@
 /* global app:true ipcRenderer:true kongConfig:true */
-(function (angular, app, ipcRenderer, kongConfig) { 'use strict';
-
-    var controller = 'InitialSetupController';
-
+(function (window, angular, app, ipcRenderer, kongConfig) { 'use strict';
+    const controller = 'InitialSetupController';
     if (typeof app === 'undefined') throw (controller + ': app is undefined');
 
-    app.controller(controller, ['$scope', '$element', '$base64', 'ajax', 'toast', function ($scope, $element, $base64, ajax, toast) {
-        var statusBar = $element.find('footer.footer').children('span');
+    app.controller(controller, ['$scope', '$element', '$base64', 'ajax', 'toast',
+        function ($scope, $element, $base64, ajax, toast) {
+
+        let statusBar = $element.find('footer.footer').children('span');
         statusBar.html('Reading configuration...');
 
         $scope.kongConfig = kongConfig;
         $scope.version = ipcRenderer.sendSync('get-config', 'VERSION');
 
-        var form    = $element.find('form#configForm');
-        var connect = function (config, writeConfig) {
+        let form = $element.find('form#configForm');
+        let connect = function (config, writeConfig) {
             if ($scope.kongConfig.host.charAt($scope.kongConfig.host.length - 1) === '/') {
                 $scope.kongConfig.host = $scope.kongConfig.host.substring(0, $scope.kongConfig.host.length - 1);
             }
 
             ajax.get(config).then(function (response) {
-
                 try {
-                    if (typeof response.data != 'object' || typeof response.data.version === 'undefined') {
+                    if (typeof response.data !== 'object' || typeof response.data.version === 'undefined') {
                         toast.error('Could not detect Kong Admin API running on the provided URL');
                         if (form.hasClass('hidden')) form.fadeIn(400);
                         return;
                     }
-
                 } catch (e) {
                     toast.error('Could not detect Kong Admin API running on the provided URL');
                     if (form.hasClass('hidden')) form.fadeIn(400);
@@ -36,17 +34,6 @@
                 if (writeConfig === true) {
                     ipcRenderer.send('write-config', { name: 'kong', config: $scope.kongConfig });
 
-                    ipcRenderer.on('write-config-success', function () {
-                        $element.fadeOut({ duration: 300, complete: function () { window.location.href = 'index.html'; } });
-
-                    }).on('write-config-error', function (event, arg) {
-                        toast.error(arg.message);
-
-                        var interval = setInterval(function () {
-                            clearInterval(interval);
-                            window.location.href = 'index.html';
-                        }, 2000);
-                    });
                 } else {
                     $element.fadeOut({ duration: 300, complete: function () { window.location.href = 'index.html'; } });
                 }
@@ -54,18 +41,31 @@
             }, function (response) {
                 if (form.hasClass('hidden')) form.fadeIn(400);
 
-                if (response.status && parseInt(response.status) === 401)
+                if (response.status && 401 === parseInt(response.status)) {
                     toast.error('Please provide correct username and password');
 
-                else
+                } else {
                     toast.error('Could not connect to ' + $scope.kongConfig.host);
+                }
             });
         };
+
+        ipcRenderer.on('write-config-success', function () {
+            $element.fadeOut({ duration: 300, complete: function () { window.location.href = 'index.html'; } });
+
+        }).on('write-config-error', function (event, arg) {
+            toast.error(arg.message);
+
+            let interval = setInterval(function () {
+                clearInterval(interval);
+                window.location.href = 'index.html';
+            }, 2000);
+        });
 
         form.on('submit', function (event) {
             event.preventDefault();
 
-            var config = {url: $scope.kongConfig.host, headers: {}};
+            let config = {url: $scope.kongConfig.host, headers: {}};
 
             if ($scope.kongConfig.username) {
                 config.headers['Authorization'] = 'Basic ' + $base64.encode($scope.kongConfig.username + ':' + ($scope.kongConfig.password || ''));
@@ -75,7 +75,7 @@
             return false;
         });
 
-        var timeout = setInterval(function () {
+        let timeout = setInterval(function () {
             statusBar.html('');
             $element.find('.icon').slideUp({ duration: 300 });
 
@@ -89,4 +89,4 @@
         }, 2000);
     }]);
 
-})(window.angular, app, ipcRenderer, kongConfig);
+})(window, window.angular, app, ipcRenderer, kongConfig);
