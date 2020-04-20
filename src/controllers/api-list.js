864923030c9e8c333cb8b227f559f3c9726cd3e1
Noman Rafaquat <Noman.Rafaquat@tpsonline.com>
2020-04-20 16:29:32 +0500

    remove testing code

--- src/controllers/api-list.js
+++ src/controllers/api-list.js
@@ -71,36 +71,6 @@
             }
         }
 
-        $scope.saveTextAsFile  = function (data, filename){
-
-                if(!data) {
-                    console.error('Console.save: No data')
-                    return;
-                }
-        
-                if(!filename) filename = 'console.json'
-        
-                var blob = new Blob([data], {type: 'text/plain'}),
-                    e    = document.createEvent('MouseEvents'),
-                    a    = document.createElement('a')
-        // FOR IE:
-        
-        if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-            window.navigator.msSaveOrOpenBlob(blob, filename);
-        }
-        else{
-            var e = document.createEvent('MouseEvents'),
-                a = document.createElement('a');
-        
-            a.download = filename;
-            a.href = window.URL.createObjectURL(blob);
-            a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
-            e.initEvent('click', true, false, window,
-                0, 0, 0, 0, 0, false, false, false, false, 0, null);
-            a.dispatchEvent(e);
-        }
-        }
-
         let panelAdd = angular.element('div#panelAdd');
         let apiForm = panelAdd.children('div.panel__body').children('form');
 
