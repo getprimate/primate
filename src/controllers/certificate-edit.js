e7ba4bd16eb9d346ebed936d8e091f77a55f8351
Ajay Sreedhar <sree.ajay468@gmail.com>
2021-12-28 09:45:05 +0530

    remove: replace deleteAction with actionButtons

--- src/controllers/certificate-edit.js
+++ src/controllers/certificate-edit.js
@@ -5,26 +5,29 @@
     if (typeof app === 'undefined') throw (controller + ': app is undefined');
 
     app.controller(controller, ['$window', '$scope', '$routeParams', 'ajax', 'viewFactory', 'toast',
-        function ($window, $scope, $routeParams, ajax, viewFactory, toast) {
+        function ($window, scope, routeParams, ajax, viewFactory, toast) {
 
         viewFactory.title = 'Edit Certificate';
+        viewFactory.actionButtons.splice(0);
 
-        $scope.certId = $routeParams.certificateId;
-        $scope.formInput = {};
-        $scope.sniList = [];
+        scope.certId = routeParams.certificateId;
+        scope.formInput = {};
+        scope.sniList = [];
 
-        ajax.get({ resource: '/certificates/' + $scope.certId }).then(function (response) {
-            $scope.formInput.certificate = response.data.cert;
-            $scope.formInput.privateKey = response.data.key;
+        ajax.get({ resource: '/certificates/' + scope.certId }).then(function (response) {
+            scope.formInput.certificate = response.data.cert;
+            scope.formInput.privateKey = response.data.key;
 
-            $scope.sniList = (typeof response.data.snis === 'object'
+            scope.sniList = (typeof response.data.snis === 'object'
                 && Array.isArray(response.data.snis)) ? response.data.snis : [];
 
-            viewFactory.deleteAction = {
+            viewFactory.actionButtons.push({
                 target: 'Certificate',
-                url: '/certificates/' + $scope.certId,
-                redirect: '#!/certificates'
-            };
+                url: `/certificates/${scope.certId}`,
+                redirect: '#!/certificates',
+                styles: 'btn danger delete',
+                displayText: 'Delete'
+            });
 
         }, function () {
             toast.error('Could not load certificate details');
@@ -38,16 +41,16 @@
 
             let payload = {};
 
-            if ($scope.formInput.certificate.trim().length > 10) {
-                payload.cert = $scope.formInput.certificate;
+            if (scope.formInput.certificate.trim().length > 10) {
+                payload.cert = scope.formInput.certificate;
 
             } else {
                 formEdit.find('textarea[name="certificate"]').focus();
                 return false;
             }
 
-            if ($scope.formInput.privateKey.trim().length > 10) {
-                payload.key = $scope.formInput.privateKey;
+            if (scope.formInput.privateKey.trim().length > 10) {
+                payload.key = scope.formInput.privateKey;
 
             } else {
                 formEdit.find('textarea[name="privateKey"]').focus();
@@ -55,7 +58,7 @@
             }
 
             ajax.patch({
-                resource: '/certificates/' + $scope.certId,
+                resource: '/certificates/' + scope.certId,
                 data: payload
             }).then(function() {
                 toast.success('Certificate updated');
@@ -78,14 +81,14 @@
             }
 
             payload.name = hostInput.val();
-            payload.ssl_certificate_id = $scope.certId;
+            payload.ssl_certificate_id = scope.certId;
 
             ajax.post({
                 resource: '/snis/',
                 data: payload
             }).then(function () {
                 toast.success('New host name added');
-                $scope.sniList.push(hostInput.val());
+                scope.sniList.push(hostInput.val());
 
                 hostInput.val('');
 
