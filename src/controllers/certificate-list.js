8e4d41d7777f936a821505d0d059b3a90c316883
Ajay Sreedhar <sree.ajay468@gmail.com>
2021-12-30 16:07:14 +0530

    add: Declare JSDoc compatible types

--- src/controllers/certificate-list.js
+++ src/controllers/certificate-list.js
@@ -1,108 +1,114 @@
 'use strict';
 
-export default function CertificateListController (window, scope, ajax, viewFrame, toast) {
-    const { angular } = window;
+import utils from '../lib/utils.js';
+
+/**
+ * Provides controller constructor for listing all certificates and SNIs.
+ *
+ * @constructor
+ *
+ * @param {Window} window- window DOM object
+ * @param {{
+ *      certList: [Object], caList: [Object], sniList: [Object],
+ *      certNext: string, caNext: string, sniNext: string,
+ *      fetchCertList: function, fetchCaList: function, fetchSniList: function
+ *      }} scope
+ * @param {AjaxProvider} ajax - custom AJAX provider
+ * @param {ViewFrameFactory} viewFrame - custom view frame factory
+ * @param {ToastFactory} toast - custom toast message service
+ */
+export default function CertificateListController(window, scope, ajax, viewFrame, toast) {
+    scope.certList = [];
+    scope.certNext = '';
+    scope.caList = [];
+    scope.caNext = '';
+    scope.sniList = [];
+    scope.sniNext = '';
+
+    /**
+     * Retrieves certificates from the specified resource.
+     *
+     * @param {string} resource - the resource identifier
+     * @returns {boolean} - true on success
+     */
+    scope.fetchCertList = (resource) => {
+        ajax.get({resource})
+            .then(({data: response}) => {
+                scope.certNext = (typeof response.next === 'string') ? response.next.replace(new RegExp(viewFrame.host), '') : '';
+
+                for (let certificate of response.data) {
+                    certificate.name = utils.objectName(certificate.id);
+                    certificate.tags = (certificate.tags.length >= 1) ? certificate.tags.join(', ') : 'No tags added';
+
+                    scope.certList.push(certificate);
+                }
+            })
+            .catch(() => {
+                toast.error('Could not load certificates');
+            });
+
+        return true;
+    };
+
+    /**
+     * Retrieves CAs from the specified resource.
+     *
+     * @param {string} resource - the resource identifier
+     * @returns {boolean} - true on success
+     */
+    scope.fetchCaList = (resource) => {
+        ajax.get({resource})
+            .then(({data: response}) => {
+                scope.caNext = (typeof response.next === 'string') ? response.next.replace(new RegExp(viewFrame.host), '') : '';
+
+                for (let ca of response.data) {
+                    ca.name = utils.objectName(ca.id);
+                    ca.tags = (ca.tags.length >= 1) ? ca.tags.join(', ') : 'No tags added';
+
+                    scope.caList.push(ca);
+                }
+            });
+
+        return true;
+    };
+
+    /**
+     * Retrieves SNI objects from the specified resource.
+     *
+     * @param {string} resource - the resource identifier
+     * @returns {boolean} - true on success
+     */
+    scope.fetchSniList = (resource) => {
+        ajax.get({resource})
+            .then(({data: response}) => {
+                scope.sniNext = (typeof response.next === 'string') ? response.next.replace(new RegExp(viewFrame.host), '') : '';
+
+                for (let sni of response.data) {
+                    if (typeof sni.certificate !== 'object' || sni.certificate === null) {
+                        sni.certificate = {id: null};
+                    }
+
+                    sni.certificate.name = utils.objectName(sni.certificate.id);
+                    scope.sniList.push(sni);
+                }
+            })
+            .catch(({data: error}) => {
+                toast.error(`Could not load SNIs. ${error.message}`);
+            });
+
+        return true;
+    };
 
     viewFrame.title = 'Certificate List';
-    viewFrame.prevUrl = null;
+    viewFrame.prevUrl = '';
 
     viewFrame.actionButtons.splice(0);
     viewFrame.actionButtons.push(
-        { displayText: 'Create Certificate', target: '', url: '', redirect: '', styles: 'btn info' },
-        { displayText: 'Add Certificate', target: '', url: '', redirect: '', styles: 'btn info' }
+        {displayText: 'Add Certificate', target: 'certificate', url: '/certificates', redirect: '#!/certificates/__create__', styles: 'btn success create'},
+        {displayText: 'Add Trusted CA', target: 'CA', url: '/ca_certificates', redirect: '#!/trusted-cas/__create__', styles: 'btn success create'}
     );
 
-    scope.certList = [];
-    scope.formInput = {
-        primaryCert: '',
-        primaryKey: '',
-        alternateCert: '',
-        alternateKey: '',
-        tags: '',
-        snis: []
-    };
-
-    scope.fetchCertList = (resource) => {
-        ajax.get({ resource: resource }).then((response) => {
-            const { data } = response;
-            const { data: certList } = data;
-
-            scope.nextUrl = (typeof data.next === 'string') ?
-                data.next.replace(new RegExp(viewFrame.host), '') : '';
-
-            for (let index = 0; index < certList.length; index++ ) {
-                scope.certList.push(certList[index]);
-            }
-
-        }, () => {
-            toast.error('Could not load certificates');
-        });
-    };
-
-    const panelAdd = angular.element('div#panelAdd');
-    const certForm = panelAdd.children('div.panel__body').children('form');
-
-    panelAdd.children('div.panel__heading').on('click', () => {
-        certForm.slideToggle(300);
-    });
-
-    certForm.on('submit', (event) => {
-        event.preventDefault();
-
-        const primaryCert = scope.formInput.primaryCert.trim();
-        const primaryKey = scope.formInput.primaryKey.trim();
-
-        if (primaryCert.length < 10) {
-            certForm.find('textarea#ce-ls__t01').focus();
-            return false;
-        }
-
-        if (primaryKey.length < 10) {
-            certForm.find('textarea#ce-ls__t02').focus();
-            return false;
-        }
-
-        const payload = {
-            cert: primaryCert,
-            key: primaryKey,
-            snis: []
-        };
-
-        if (scope.formInput.snis.length > 0) {
-            const sniList = scope.formInput.snis.split(',');
-
-            for (let sni of sniList) {
-                let sanitized = sni.trim();
-
-                if (sanitized.length > 0) {
-                    payload.snis.push(sni);
-                }
-            }
-        }
-
-        if (scope.formInput.alternateCert.length > 10) payload.cert_alt = scope.formInput.alternateCert.trim();
-        if (scope.formInput.alternateKey.length > 10) payload.key_alt = scope.formInput.alternateKey.trim();
-
-        console.log('Payload: ' + JSON.stringify(payload, null, 4));
-
-        ajax.post({
-            resource: '/certificates',
-            data: payload
-        }).then((response) => {
-            scope.certList.push(response.data);
-            toast.success('Added new certificate');
-
-        }, (response) => {
-            console.error('Error: ', JSON.stringify(response.data, null, 4));
-            toast.error(response.data);
-        });
-
-        return false;
-    });
-
-    certForm.on('click', 'button#ce-ls__b01', () => {
-        certForm.slideUp(300);
-    });
-
     scope.fetchCertList('/certificates');
+    scope.fetchCaList('/ca_certificates');
+    scope.fetchSniList('/snis');
 }
