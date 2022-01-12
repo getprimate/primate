77f307da0c6b1ef8dc5dc68673ea5182e31a6496
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-01-12 21:35:31 +0530

    add: JSDoc type definitions for App_ServiceModel

--- src/models/service-model.js
+++ src/models/service-model.js
@@ -1,5 +1,28 @@
+/**
+ * Copyright (c) Ajay Sreedhar. All rights reserved.
+ *
+ * Licensed under the MIT License.
+ * Please see LICENSE file located in the project root for more information.
+ */
+
+/**
+ * @typedef {Object} App_ServiceModel - The service model object
+ *
+ * @property {string} [name] - The Service name.
+ * @property {number} [retries] - The number of retries to execute upon failure to proxy. Default: 5.
+ * @property {string} protocol - The protocol used to communicate with the upstream.
+ * @property {string} host - The host of the upstream server. Note that the host value is case-sensitive.
+ * @property {number} port - The upstream server port.
+ * @property {string} [path] - Optional: The path to be used in requests to the upstream server.
+ * @property {string} [client_certificate] - Certificate to be used as client certificate while TLS handshaking to the upstream server.
+ */
+
 'use strict';
 
+/**
+ *
+ * @type App_ServiceModel
+ */
 const ServiceModel = {
     name: '',
     retries: 5,
@@ -12,8 +35,8 @@ const ServiceModel = {
     read_timeout: 60000,
     tags: [],
     client_certificate: '',
-    tls_verify: true,
-    tls_verify_depth: null,
+    tls_verify: 'default',
+    tls_verify_depth: -1,
     ca_certificates: [],
     enabled: true
 };
