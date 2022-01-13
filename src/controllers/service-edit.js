e1d085c5e37d2d47f5a64b3dddc2f2c485992d9d
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-01-13 17:28:46 +0530

    TODO: Enable support for activating plugins from the list

--- src/controllers/service-edit.js
+++ src/controllers/service-edit.js
@@ -1,43 +1,72 @@
+/**
+ * Copyright (c) Ajay Sreedhar. All rights reserved.
+ *
+ * Licensed under the MIT License.
+ * Please see LICENSE file located in the project root for more information.
+ */
+
 'use strict';
 
 import utils from '../lib/utils.js';
 import ServiceModel from '../models/service-model.js';
 
+/**
+ * Holds the list of available protocols and their configuration,
+ *
+ * The configuration also specifies the array of fields to be removed from
+ * the service object payload to avoid validation errors.
+ *
+ * @type {{tcp: {excluded: string[]}, udp: {excluded: string[]},
+ * tls_passthrough: {excluded: string[]}, http: {excluded: string[]},
+ * tls: {excluded: string[]}, https: {excluded: *[]}, grpc: {excluded: string[]}}}
+ */
 const ENUM_PROTOCOL = {
-    'http': {excluded: ['ca_certificates', 'tls_verify', 'tls_verify_depth']},
-    'grpc': {excluded: ['ca_certificates', 'tls_verify', 'tls_verify_depth', 'path']},
-    'tcp': {excluded: ['ca_certificates', 'tls_verify', 'tls_verify_depth', 'path']},
-    'udp': {excluded: ['ca_certificates', 'tls_verify', 'tls_verify_depth', 'path']},
-    'tls': {excluded: ['ca_certificates', 'tls_verify', 'tls_verify_depth', 'path']},
-    'tls_passthrough': {excluded: ['ca_certificates', 'tls_verify', 'tls_verify_depth']}
+    http: {excluded: ['ca_certificates', 'tls_verify', 'tls_verify_depth']},
+    https: {excluded: []},
+    grpc: {excluded: ['ca_certificates', 'tls_verify', 'tls_verify_depth', 'path']},
+    tcp: {excluded: ['ca_certificates', 'tls_verify', 'tls_verify_depth', 'path']},
+    udp: {excluded: ['ca_certificates', 'tls_verify', 'tls_verify_depth', 'path']},
+    tls: {excluded: ['ca_certificates', 'tls_verify', 'tls_verify_depth', 'path']},
+    tls_passthrough: {excluded: ['ca_certificates', 'tls_verify', 'tls_verify_depth']}
 };
 
-const _buildServiceModel = (model, source = {}) => {
-    const keys = Object.keys(source);
-
-    for (let key of keys) {
-        if ((typeof model[key] === 'undefined')
-            || (Array.isArray(model[key]) && source[key] === null)) {
+/**
+ * Populates Service model after sanitising values in the service object.
+ *
+ * @private
+ * @see https://docs.konghq.com/gateway/2.7.x/admin-api/#service-object
+ *
+ * @param {App_ServiceModel} model - A service model object.
+ * @param {Object} source - A service object.
+ * @return {App_ServiceModel} The populated service model.
+ */
+const _populateServiceModel = (model, source = {}) => {
+    for (let property in source) {
+        if ((typeof model[property] === 'undefined')
+            || (Array.isArray(model[property]) && source[property] === null)) {
             continue;
         }
 
-        switch (key) {
+        switch (property) {
             case 'tls_verify':
+                model[property] = (source[property] === null) ? 'default' : String(source[property]);
+                break;
+
             case 'tls_verify_depth':
-                model[key] = String(source[key]);
+                model[property] = (source[property] === null) ? -1 : source[property];
                 break;
 
             case 'client_certificate':
-                model[key] = (source[key] !== null && typeof source[key]['id'] === 'string') ? source[key]['id'] : '';
+                model[property] = (source[property] !== null && typeof source[property]['id'] === 'string') ? source[property]['id'] : '';
                 break;
 
             case 'ca_certificates':
             case 'tags':
-                model[key] = Array.isArray(source[key]) ? source[key] : [];
+                model[property] = Array.isArray(source[property]) ? source[property] : [];
                 break;
 
             default:
-                model[key] = source[key];
+                model[property] = source[property];
                 break;
         }
     }
@@ -46,25 +75,72 @@ const _buildServiceModel = (model, source = {}) => {
 };
 
 /**
+ * Prepares service object after sanitising values in a specified service model.
+ *
+ * Technically, this function does the inverse of {@link _populateServiceModel} function.
+ * The function validates service model before preparing the payload. Throws an error
+ * if the validation fails.
+ *
+ * @private
+ * @see https://docs.konghq.com/gateway/2.7.x/admin-api/#service-object
+ *
+ * @param {App_ServiceModel} model - The source service model
+ * @return {Object} The prepared service object
+ */
+const _prepareServiceObject = (model) => {
+    if (model.host.length === 0) {
+        throw new Error('Please provide a valid host');
+    }
+
+    const payload = Object.assign({}, model);
+    const {excluded} = ENUM_PROTOCOL[payload.protocol];
+
+    delete payload.client_certificate;
+
+    if (model.client_certificate.length > 10) {
+        payload.client_certificate = {id: model.client_certificate};
+    }
+
+    switch (model.tls_verify) {
+        case 'true':
+        case 'false':
+            payload.tls_verify = (model.tls_verify === 'true');
+            break;
+
+        default:
+            payload.tls_verify = null;
+    }
+
+    payload.tls_verify_depth = (model.tls_verify_depth === -1) ? null : model.tls_verify_depth;
+
+    if (Array.isArray(excluded)) {
+        for (let field of excluded) {
+            delete payload[field];
+        }
+    }
+
+    return payload;
+};
+
+/**
  * Provides controller constructor for editing CA certificates.
  *
  * @constructor
  *
- * @param {Window} window- window DOM object
- * @param {{
- *      ENUM_PROTOCOL: [string],
- *      serviceId: string,
- *      serviceModel: App_ServiceModel,
- *      pbCertList: [Object], caCertList: [Object], routeList: [Object], pluginList: [Object],
- *      fetchPublicCertificates: function, fetchCACertificates: function, fetchRoutes: function,
- *      submitServiceForm: function, resetServiceForm: function,
- *      }} scope - injected scope object
- * @param {{path: function}} location - injected location service
- * @param {{serviceId: string}} routeParams - injected route parameters service
- * @param {AjaxProvider} ajax - custom AJAX provider
- * @param {ViewFrameFactory} viewFrame - custom view frame factory
- * @param {ToastFactory} toast - custom toast message service
- * @param {LoggerFactory} logger - custom logger factory service
+ * @param {Window} window- The top level Window object
+ * @param {Object} scope - The injected scope object.
+ * @param {Object} location - Injected location service
+ * @param {function} location.path - Tells the current view path
+ * @param {Object} routeParams - Injected route parameters service.
+ * @param {string} routeParams.serviceId - The service id in editing mode.
+ * @param {AjaxProvider} ajax - Custom AJAX provider.
+ * @param {ViewFrameFactory} viewFrame - Custom view frame factory.
+ * @param {ToastFactory} toast - Custom toast message service.
+ * @param {LoggerFactory} logger - Custom logger factory service.
+ *
+ * @property {string[]} scope.ENUM_PROTOCOL - An array of protocols from {@link ENUM_PROTOCOL}.
+ * @property {string} scope.serviceId - Holds the service object id in edit mode.
+ * @property {App_ServiceModel} scope.serviceModel - Holds the service model object.
  */
 export default function ServiceEditController(window, scope, location, routeParams, ajax, viewFrame, toast, logger) {
     const {angular} = window;
@@ -181,13 +257,19 @@ export default function ServiceEditController(window, scope, location, routePara
         return true;
     };
 
+    /**
+     * Handles form submit event.
+     *
+     * The service object payload is prepared and POST or PATCH
+     * requests are triggered create or edit mode respectively.
+     *
+     * @param {Object} event - The current event object.
+     * @return {boolean} True if the request could be made, false otherwise
+     */
     scope.submitServiceForm = (event) => {
         if (typeof event ==='undefined') {
             return false;
         }
-
-        /** @type {AngularElement} */
-        const formService = angular.element(event.target);
 
         event.preventDefault();
 
@@ -195,31 +277,8 @@ export default function ServiceEditController(window, scope, location, routePara
             if (typeof scope.serviceModel[key] === 'string') scope.serviceModel[key] = scope.serviceModel[key].trim();
         });
 
-        if (scope.serviceModel.host.length === 0) {
-            formService.find('input#sv-ed__txt01').focus();
-            return false;
-        }
-
-        const payload = angular.copy(scope.serviceModel);
-        const {excluded} = ENUM_PROTOCOL[payload.protocol];
-
-        delete payload.client_certificate;
-
-        if (scope.serviceModel.client_certificate.length > 10) {
-            payload.client_certificate = {id: scope.serviceModel.client_certificate};
-        }
-
-        if (Array.isArray(excluded)) {
-            for (let field of excluded) {
-                delete payload[field];
-            }
-        }
-
-        const request = ajax.request({
-            method: ajaxConfig.method,
-            resource: ajaxConfig.resource,
-            data: payload
-        });
+        const payload = _prepareServiceObject(scope.serviceModel);
+        const request = ajax.request({ method: ajaxConfig.method, resource: ajaxConfig.resource, data: payload });
 
         request.then(({data: response, config: httpConfig, status: statusCode, statusText}) => {
             logger.info({ source: 'http-response', httpConfig, statusCode, statusText });
@@ -252,8 +311,7 @@ export default function ServiceEditController(window, scope, location, routePara
      * @return boolean - True if reset confirmed, false otherwise
      */
     scope.resetServiceForm = (event) => {
-        if (confirm('This will clear the values from the form. Proceed?')) {
-
+        if (confirm('Proceed to clear the form?')) {
             scope.serviceModel = angular.copy(ServiceModel);
             return true;
         }
@@ -266,7 +324,7 @@ export default function ServiceEditController(window, scope, location, routePara
         const request = ajax.get({resource: ajaxConfig.resource});
 
         request.then(({ data: response }) => {
-            _buildServiceModel(scope.serviceModel, response);
+            _populateServiceModel(scope.serviceModel, response);
 
             viewFrame.actionButtons.push({
                 target: 'service',
