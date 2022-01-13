d7aea57de1a17cb2e7609f131eaeb11c56cbf826
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-01-13 17:35:33 +0530

    add: JSDoc properties for App_ServiceModel

--- src/models/service-model.js
+++ src/models/service-model.js
@@ -10,23 +10,30 @@
 /**
  * @typedef {Object} App_ServiceModel - The service model object
  *
- * @property {string} [name] - The Service name.
- * @property {number} [retries] - The number of retries to execute upon failure to proxy. Default: 5.
- * @property {string} protocol - The protocol used to communicate with the upstream.
  * @property {string} host - The host of the upstream server. Note that the host value is case-sensitive.
  * @property {number} port - The upstream server port.
- * @property {string} [path] - Optional: The path to be used in requests to the upstream server.
- * @property {string} [client_certificate] - Certificate to be used as client certificate while TLS handshaking to the upstream server.
- * @property {string[]} [tags] - An optional set of strings associated with the Service for grouping and filtering.
+ * @property {string} name - The Service name.
+ * @property {boolean} enabled - Whether the Service is active.
+ * @property {number} retries - The number of retries to execute upon failure to proxy. Default: 5.
+ * @property {string} protocol - The protocol used to communicate with the upstream.
+ * @property {string} path - The path to be used in requests to the upstream server.
+ * @property {number} connect_timeout - The timeout in milliseconds for establishing a connection to the upstream server.
+ * @property {number} write_timeout - The timeout in milliseconds between two successive write operations for transmitting a request to the upstream server.
+ * @property {number} read_timeout - The timeout in milliseconds between two successive read operations for transmitting a request to the upstream server.
+ * @property {string} client_certificate - Certificate to be used as client certificate while TLS handshaking to the upstream server.
+ * @property {string} tls_verify - Whether to enable verification of upstream server TLS certificate.
+ * @property {number} tls_verify_depth - Maximum depth of chain while verifying Upstream server’s TLS certificate.
+ * @property {string[]} tags - An optional set of strings associated with the Service for grouping and filtering.
  */
 
 /**
- * Defines the service object.
+ * Defines the service object with default values.
  *
  * @type App_ServiceModel
  */
 const ServiceModel = {
     name: '',
+    enabled: true,
     retries: 5,
     protocol: 'http',
     host: '',
@@ -35,12 +42,11 @@ const ServiceModel = {
     connect_timeout: 60000,
     write_timeout: 60000,
     read_timeout: 60000,
-    tags: [],
     client_certificate: '',
-    tls_verify: 'default',
+    tls_verify: 'nginx',
     tls_verify_depth: -1,
     ca_certificates: [],
-    enabled: true
+    tags: [],
 };
 
 export default ServiceModel;
