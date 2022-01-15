c04e04a6cf3d38a064c2f388110c9fb98e3cd29e
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-01-15 16:12:01 +0530

    add: Syn linting and code formatting rules

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    singleQuote: true,
+    useTabs: false,
+    tabWidth: 4,
+    semi: true,
+    trailingComma: 'none',
+    bracketSpacing: false,
+    bracketSameLine: true,
+    arrowParens: 'always'
+};
