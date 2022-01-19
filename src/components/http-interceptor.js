0de29a8e80e80c2853d3a956c063a2313e2b14f4
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-01-19 11:16:37 +0530

    TODO: Remove headerText usages

--- src/components/http-interceptor.js
+++ src/components/http-interceptor.js
@@ -10,12 +10,26 @@
 function _httpInterceptor($q) {
     return {
         response(result) {
-            result.configText = `${result.status} ${result.statusText} - ${result.config.method} ${result.config.url}`;
+            result.httpText = `${result.status} ${result.statusText} - ${result.config.method} ${result.config.url}`;
+
+            /**
+             * @deprecated Use httpText instead.
+             *
+             * TODO : Remove headerText usages.
+             */
+            result.headerText = `${result.status} ${result.statusText} - ${result.config.method} ${result.config.url}`;
             return result;
         },
 
         responseError(error) {
-            error.configText = `${error.status} ${error.statusText} - ${error.config.method} ${error.config.url}`;
+            error.httpText = `${error.status} ${error.statusText} - ${error.config.method} ${error.config.url}`;
+
+            /**
+             * @deprecated Use httpText instead.
+             *
+             * TODO : Remove headerText usages.
+             */
+            error.headerText = `${error.status} ${error.statusText} - ${error.config.method} ${error.config.url}`;
             return $q.reject(error);
         }
     };
