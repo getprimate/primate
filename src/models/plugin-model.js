74fac6841bcdd0eb1d839045db774a9ea860e8a1
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-01-19 16:32:50 +0530

    add: Model definition for plugin scope

---
+++ src/models/plugin-model.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Ajay Sreedhar. All rights reserved.
+ *
+ * Licensed under the MIT License.
+ * Please see LICENSE file located in the project root for more information.
+ */
+
+'use strict';
+
+const PluginModel = {
+    name: '__none__',
+    route: '__none__',
+    service: '__none__',
+    consumer: '__none__',
+    config: {},
+    protocols: ['http', 'https', 'grpc', 'grpcs'],
+    enabled: true,
+    tags: []
+};
+
+export default PluginModel;
