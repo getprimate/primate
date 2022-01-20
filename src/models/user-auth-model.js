0d817652b66fbcb2db1f6295faef103bea7fc85d
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-01-20 13:54:01 +0530

    fix: Refactor with prettier

---
+++ src/models/user-auth-model.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) Ajay Sreedhar. All rights reserved.
+ *
+ * Licensed under the MIT License.
+ * Please see LICENSE file located in the project root for more information.
+ */
+
+'use strict';
+
+const UserAuthModel = {
+    keyAuth: {
+        key: '',
+        ttl: 0,
+        tags: []
+    },
+    basicAuth: {
+        username: '',
+        password: '',
+        tags: []
+    },
+    oauth2: {
+        name: '',
+        client_id: '',
+        client_secret: '',
+        client_type: '',
+        hash_secret: '',
+        redirect_uris: [],
+        tags: []
+    },
+    hmacAuth: {
+        username: '',
+        secret: '',
+        tags: []
+    },
+    jwt: {
+        algorithm: 'HS256',
+        key: '',
+        rsa_public_key: '',
+        secret: '',
+        tags: []
+    },
+    acls: {
+        group: '',
+        tags: []
+    }
+};
+
+export default UserAuthModel;
