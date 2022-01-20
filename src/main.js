9a59a766f53d485fa757bcecffdd550f125ae147
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-01-20 23:36:21 +0530

    change: Use loadFile to load bootstrap view

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@
 const APP_NAME = 'KongDash';
 const VERSION = '0.3.0';
 
-const electron  = require('electron');
-const path      = require('path');
+const electron = require('electron');
+const path = require('path');
 const ospath = require('ospath');
-const jsonfile  = require('jsonfile');
+const jsonfile = require('jsonfile');
 
-let absPath = path.dirname(__dirname), configFile = ospath.data() + '/' + APP_NAME + '/config.json';
+let absPath = path.dirname(__dirname),
+    configFile = ospath.data() + '/' + APP_NAME + '/config.json';
 let {app, ipcMain, BrowserWindow, Menu} = electron;
-let mainWindow, appConfig = {kong: {}, app: {enableAnimation: true}};
+let mainWindow,
+    appConfig = {kong: {}, app: {enableAnimation: true}};
 
 let startMainWindow = function () {
     mainWindow = new BrowserWindow({
@@ -24,10 +26,10 @@ let startMainWindow = function () {
         icon: absPath + '/kongdash-256x256.png',
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false,
+            contextIsolation: false
         }
     });
-    mainWindow.loadURL('file://' + absPath + '/src/initialize.html');
+    mainWindow.loadFile(`${absPath}/src/dashboard/initialize.html`);
 
     //* Debugging
     mainWindow.webContents.openDevTools();
@@ -43,7 +45,6 @@ app.setName(APP_NAME);
 app.on('ready', () => {
     try {
         appConfig = jsonfile.readFileSync(configFile);
-
     } catch (e) {
         /* Ignore. Uses default settings. */
     } finally {
@@ -56,54 +57,74 @@ app.on('activate', () => {
 });
 
 app.on('browser-window-created', (e, window) => {
-    let menuTemplate = [{
-        label: 'File',
-        submenu: [{
-            label: 'Settings',
-            click: () => {
-                mainWindow.webContents.send('open-settings-view', '');
-            }
-        }, { type: 'separator' }, { role: 'quit' }]
-    }, {
-        label: 'Edit',
-        submenu: [{ role: 'undo' }, { role: 'redo' }, { type: 'separator' }, { role: 'cut' }, { role: 'copy' }, { role: 'paste' }]
-    }, {
-        label: 'Window',
-        submenu: [{role: 'togglefullscreen'}]
-    }, {
-        label: 'Help',
-        submenu: [{
-            label: 'GitHub Repository',
-            click: () => {
-                electron.shell.openExternal('https://github.com/ajaysreedhar/KongDash');
-            }
-        }, {
-            label: 'Report Issues',
-            click: () => {
-                electron.shell.openExternal('https://github.com/ajaysreedhar/KongDash/issues');
-            }
-        }, {
-            type: 'separator'
-        }, {
-            label: 'About KongDash',
-            click: () => {
-                electron.shell.openExternal('https://ajaysreedhar.github.io/KongDash/');
-            }
-        }]
-    }];
+    let menuTemplate = [
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Settings',
+                    click: () => {
+                        mainWindow.webContents.send('open-settings-view', '');
+                    }
+                },
+                {type: 'separator'},
+                {role: 'quit'}
+            ]
+        },
+        {
+            label: 'Edit',
+            submenu: [
+                {role: 'undo'},
+                {role: 'redo'},
+                {type: 'separator'},
+                {role: 'cut'},
+                {role: 'copy'},
+                {role: 'paste'}
+            ]
+        },
+        {
+            label: 'Window',
+            submenu: [{role: 'togglefullscreen'}]
+        },
+        {
+            label: 'Help',
+            submenu: [
+                {
+                    label: 'GitHub Repository',
+                    click: () => {
+                        electron.shell.openExternal('https://github.com/ajaysreedhar/KongDash');
+                    }
+                },
+                {
+                    label: 'Report Issues',
+                    click: () => {
+                        electron.shell.openExternal('https://github.com/ajaysreedhar/KongDash/issues');
+                    }
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'About KongDash',
+                    click: () => {
+                        electron.shell.openExternal('https://ajaysreedhar.github.io/KongDash/');
+                    }
+                }
+            ]
+        }
+    ];
 
     let menu = Menu.buildFromTemplate(menuTemplate);
 
     if (process.platform === 'darwin' || process.platform === 'mas') {
         Menu.setApplicationMenu(menu);
-
     } else {
         window.setMenu(menu);
     }
 });
 
 app.on('window-all-closed', () => {
-    if(process.platform !== 'darwin') app.quit();
+    if (process.platform !== 'darwin') app.quit();
 });
 
 ipcMain.on('get-config', (event, arg) => {
@@ -121,7 +142,6 @@ ipcMain.on('write-config', (event, arg) => {
     jsonfile.writeFile(configFile, appConfig, function (error) {
         if (error) {
             event.sender.send('write-config-error', {message: 'Could not write configuration file.' + error});
-
         } else {
             event.sender.send('write-config-success', {message: 'Configuration saved successfully'});
         }
