c3b62c06684d00ee908c4d35bb962693d4fddb4c
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-01-20 09:58:00 +0530

    add: Move consumer model object to new file

---
+++ src/models/consumer-model.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) Ajay Sreedhar. All rights reserved.
+ *
+ * Licensed under the MIT License.
+ * Please see LICENSE file located in the project root for more information.
+ */
+
+'use strict';
+
+/**
+ * @typedef {Object} App_ConsumerModel - The consumer model to be attached to the scope.
+ *
+ * @property {string} username - The unique username of the consumer.
+ * @property {string} custom_id - Field for storing an existing unique id for the consumer.
+ * @property {string[]} tags - An optional set of strings associated with the consumer for grouping and filtering.
+ */
+
+/**
+ * Defines a consumer model with default values.
+ *
+ * @type {App_ConsumerModel}
+ */
+const ConsumerModel = {
+    username: '',
+    custom_id: '',
+    tags: []
+};
+
+export default ConsumerModel;
