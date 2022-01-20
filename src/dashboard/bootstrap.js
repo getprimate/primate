cd13c6e3f6be43cacb713f9a676693cac3ae2983
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-01-20 23:34:05 +0530

    TODO: Move InitialSetUpController to BootstrapController

---
+++ src/dashboard/bootstrap.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import KongDash from './kongdash.js';
+import BootstrapController from './controllers/bootstrap.js';
+
+KongDash.controller('BootstrapController', ['$scope', '$element', '$base64', 'ajax', 'toast', BootstrapController]);
