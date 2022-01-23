43aaa8a9b99e31b38143bfec96591cb5a363b123
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-01-23 16:29:11 +0530

    change: Register logger at http inceptor level

--- src/dashboard/controllers/upstream-list.js
+++ src/dashboard/controllers/upstream-list.js
@@ -1,32 +1,64 @@
+/**
+ * Copyright (c) Ajay Sreedhar. All rights reserved.
+ *
+ * Licensed under the MIT License.
+ * Please see LICENSE file located in the project root for more information.
+ */
+
 'use strict';
 
+import restUtils from '../../lib/rest-utils.js';
+
 /**
+ * Provides controller constructor for listing upstream objects.
  *
- * @param scope
- * @param ajax
- * @param viewFrame
- * @param toast
  * @constructor
+ * @param {Object} scope - Injected scope object.
+ * @param {RESTClientFactory} restClient - Customised HTTP REST client factory.
+ * @param {ViewFrameFactory} viewFrame - Factory for sharing UI details.
+ * @param {ToastFactory} toast - Factory for displaying notifications.
  */
-export default function UpstreamListController(scope, ajax, viewFrame, toast) {
-    viewFrame.title = 'Upstreams';
-    viewFrame.prevUrl = '';
-
+export default function UpstreamListController(scope, restClient, viewFrame, toast) {
     scope.upstreamList = [];
+    scope.upstreamNext = {offset: ''};
 
-    scope.fetchUpstreamList = (resource) => {
-        ajax.get({ resource: resource }).then(({data: response}) => {
-            scope.nextUrl = (typeof response.next === 'string') ? response.next.replace(new RegExp(viewFrame.host), '') : '';
+    /**
+     * Retrieves the list of upstream objects.
+     *
+     * @param {string|object|null} filters - Filters to the Admin API.
+     * @return {boolean} True if request could be made, false otherwise.
+     */
+    scope.fetchUpstreamList = (filters = null) => {
+        const request = restClient.get('/upstreams' + restUtils.urlQuery(filters));
+
+        viewFrame.setLoaderStep(100);
+
+        request.then(({data: response}) => {
+            scope.upstreamNext.offset = restUtils.urlOffset(response.next);
 
             for (let upstream of response.data) {
-                scope.upstreamList.push(upstream);
+                scope.upstreamList.push({
+                    id: upstream.id,
+                    name: upstream.name,
+                    algorithm: upstream.algorithm,
+                    created_at: upstream.created_at
+                });
             }
-        }).catch(() => {
+        });
+
+        request.catch(() => {
             toast.error('Could not load upstreams');
+        });
+
+        request.finally(() => {
+            viewFrame.incrementLoader();
         });
     };
 
-    viewFrame.actionButtons.push({ displayText: 'New Upstream', target: '', url: '', redirect: '#!/upstreams/__create__', styles: 'btn success create' });
+    viewFrame.setTitle('Upstreams');
+    viewFrame.clearRoutes();
+
+    viewFrame.addAction('New Upstream', '#!/upstreams/__create__');
 
     scope.fetchUpstreamList('/upstreams');
 }
