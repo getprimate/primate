985f8b93d9d49e619711f77e56b6a8557f3bf340
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-02-07 23:20:52 +0530

    change: rename test files

--- test/themes.js
+++ test/themes.js
@@ -10,6 +10,12 @@ async function testLoader() {
     let themes = await themeScanner.scanThemes();
 
     console.log(JSON.stringify(themes, null, 4));
+
+    for (let nonce of Object.keys(themes)) {
+        let styles = await themeScanner.readStyle(nonce);
+
+        console.log(nonce, '=', JSON.stringify(styles));
+    }
 }
 
 const test = testLoader();
@@ -17,3 +23,7 @@ const test = testLoader();
 test.then(() => {
     console.log('Tests completed');
 });
+
+test.catch((err) => {
+    console.error(err);
+})
