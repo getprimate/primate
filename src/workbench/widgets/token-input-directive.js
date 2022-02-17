4e41e4a2a6326d3e04b09844e83690e6ece7612b
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-02-17 11:30:04 +0530

    change: Refactor token-input-directive

--- src/workbench/widgets/token-input-directive.js
+++ src/workbench/widgets/token-input-directive.js
@@ -1,47 +1,152 @@
+/**
+ * Copyright (c) Ajay Sreedhar. All rights reserved.
+ *
+ * Licensed under the MIT License.
+ * Please see LICENSE file located in the project root for more information.
+ */
+
 'use strict';
 
-import {isNil, isText} from '../../lib/core-toolkit.js';
-
-const {document} = window;
+import {explode, isNil, isText, randomHex} from '../../lib/core-toolkit.js';
 
 /**
- * Attaches nodes under the unordered list.
+ * Creates LI nodes with token values under the specified UL element.
  *
- * @private
- * @param {{tokenList: Array}} scope - the current scope
- * @param {HTMLElement} element - the parent element
- * @param {Array|string} tokens - tokens to be appended
- * @return {{append: function}} the element object
+ * @param {HTMLUListElement} listElement - The parent UL element.
+ * @param {string[]} tokens - An array of tokens to be added.
+ * @return {string[]} The added token array.
  */
-function attachListNodes(scope, element, tokens) {
-    const shouldAppend = isText(tokens);
-    /* TODO : Skip splitting tokens by comma, if the feature is disabled using data-disable-parser="true". */
-    const tokenList = shouldAppend === true ? tokens.split(',') : [...scope.tokenList];
-
-    for (let token of tokenList) {
-        token = token.trim();
-
+function attachItemElements(listElement, tokens = []) {
+    for (let token of tokens) {
         let li = document.createElement('li');
-        li.dataset.tokenValue = token;
+
+        li.dataset.displayText = token;
+        li.dataset.identifier = randomHex();
         li.innerHTML = `${token} <span class ="material-icons">highlight_off</span>`;
 
-        element.appendChild(li);
-
-        if (shouldAppend === true) {
-            scope.tokenList.push(token);
-        }
+        listElement.appendChild(li);
     }
 
-    return element;
+    return tokens;
 }
 
 /**
- * Directive constructor for token-input widgets.
+ * Removes the specified LI node from the UL element.
  *
- * @constructor
- *
- * @returns {Object} the directive object
+ * @param {HTMLUListElement} listElement - The parent UL element.
+ * @param {HTMLLIElement} itemElement - The LI element to be removed.
+ * @return {number} The index of the removed element.
  */
+function detachItemElement(listElement, itemElement) {
+    const {identifier} = itemElement.dataset;
+    let index = 0;
+
+    for (let li of listElement.children) {
+        if (li.dataset.identifier === identifier) {
+            break;
+        }
+
+        index++;
+    }
+
+    itemElement.remove();
+
+    return index;
+}
+
+/**
+ * Removes all LI nodes from the UL element.
+ *
+ * @param {HTMLUListElement} listElement - The parent UL element.
+ * @return {number} - The number of items removed.
+ */
+function clearItemElements(listElement) {
+    const length = listElement.children.length;
+
+    for (let li of listElement.children) {
+        li.remove();
+    }
+
+    return length;
+}
+
+function link(scope, jqElement, attributes) {
+    /** @type HTMLElement */
+    const parentElement = jqElement[0];
+
+    /* Abort linking if the provided ngModel is not an array. */
+    if (!Array.isArray(scope.tokenList)) {
+        return false;
+    }
+
+    const textElement = parentElement.querySelector('textarea.token-input__text');
+    const listElement = parentElement.querySelector('ul.token-input__list');
+
+    const onTokenListUpdated = (current, previous) => {
+        clearItemElements(listElement);
+
+        if (!Array.isArray(current) || current.length === 0) {
+            return false;
+        }
+
+        if (!Array.isArray(previous) || current.length !== previous.length) {
+            attachItemElements(listElement, current);
+        }
+
+        return true;
+    };
+
+    parentElement.classList.add('token-input');
+
+    textElement.placeholder = isText(attributes.placeholder) ? attributes.placeholder : 'Type and press enter...';
+
+    textElement.addEventListener('keyup', (event) => {
+        event.preventDefault();
+
+        const {target} = event;
+
+        if (event.keyCode === 13) {
+            let value = target.value.trim();
+
+            if (value.length >= 1) {
+                const tokens = isNil(attributes['disableParser']) ? explode(value, ',') : [value];
+                const items = attachItemElements(listElement, tokens);
+
+                scope.tokenList.push(...items);
+            }
+
+            target.value = '';
+        }
+    });
+
+    listElement.addEventListener('click', (event) => {
+        const {target} = event;
+
+        if (target.nodeName !== 'LI' && target.nodeName !== 'SPAN') {
+            return false;
+        }
+
+        let itemNode = target;
+
+        if (target.nodeName === 'SPAN' && target.classList.contains('material-icons')) {
+            itemNode = target.closest('li');
+        }
+
+        const index = detachItemElement(listElement, itemNode);
+        scope.tokenList.splice(index, 1);
+    });
+
+    scope.$watch('tokenList', onTokenListUpdated, false);
+
+    scope.$on('$destroy', () => {
+        clearItemElements(listElement);
+    });
+}
+
+function controller(scope) {
+    scope.isInitialised = false;
+}
+
 export default function TokenInputDirective() {
     return {
         transclude: false,
@@ -49,107 +154,7 @@ export default function TokenInputDirective() {
         require: 'ngModel',
         template: '<textarea class="token-input__text"></textarea><ul class="token-input__list"></ul>',
         scope: {tokenList: '=ngModel'},
-        controller: [
-            '$scope',
-            function (scope) {
-                scope.isInitialised = false;
-            }
-        ],
-
-        /**
-         *
-         * @param {Object} scope - the ng-model of the element
-         * @param {{children: function, addClass: function, on: function}} element - a jqLite instance of the element
-         * @param {Object} attrs - the element attributes
-         * @returns {boolean} true on linking, false otherwise
-         */
-        link(scope, element, attrs) {
-            /** @type HTMLElement */
-            const parent = element[0];
-
-            if (!Array.isArray(scope.tokenList)) {
-                return false;
-            }
-
-            /**
-             * The textarea for typing tokens.
-             *
-             * @type {Object}
-             * @property {function} attr - sets an attribute
-             */
-            const textNode = parent.querySelector('textarea.token-input__text');
-
-            /**
-             * The unordered list for attaching tokens.
-             */
-            const itemNode = parent.querySelector('ul.token-input__list');
-
-            scope.$watch(
-                'tokenList',
-                (current, previous) => {
-                    if (scope.isInitialised === false) {
-                        scope.isInitialised = true;
-                        attachListNodes(scope, itemNode, scope.tokenList);
-
-                        return scope.isInitialised;
-                    }
-
-                    if (Array.isArray(previous) && Array.isArray(current) && previous.length !== current.length) {
-                        attachListNodes(scope, itemNode, current);
-
-                        return true;
-                    }
-
-                    return false;
-                },
-                false
-            );
-
-            parent.classList.add('token-input');
-
-            itemNode.addEventListener('click', (event) => {
-                const {target} = event;
-                let element = target;
-
-                if (target.nodeName === 'UL') {
-                    return false;
-                }
-
-                if (target.nodeName === 'SPAN' && target.classList.contains('material-icons')) {
-                    element = target.closest('li');
-                }
-
-                element.remove();
-
-                if (Array.isArray(scope.tokenList)) {
-                    scope.tokenList.splice(0);
-
-                    for (let child of itemNode.children) {
-                        let {tokenValue} = child.dataset;
-
-                        if (typeof tokenValue === 'string') {
-                            scope.tokenList.push(tokenValue);
-                        }
-                    }
-                }
-            });
-
-            textNode.placeholder = isText(attrs.placeholder) ? attrs.placeholder : 'Type and press enter...';
-
-            textNode.addEventListener('keyup', (event) => {
-                const {target} = event;
-                event.preventDefault();
-
-                if (event.keyCode === 13) {
-                    let value = target.value.trim();
-
-                    if (value.length >= 1) {
-                        attachListNodes(scope, itemNode, target.value.trim());
-                    }
-
-                    target.value = '';
-                }
-            });
-        }
+        controller: ['$scope', controller],
+        link
     };
 }
