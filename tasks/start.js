ce480902022dcddc3264c1134cee774f155a5f8e
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-03-01 23:34:22 +0530

    add: Pipe stdout and stderr

--- tasks/start.js
+++ tasks/start.js
@@ -1,23 +1,20 @@
-/**
- * @deprecated Will be removed in future
- */
+'use strict';
+
+const childProcess = require('child_process');
+const path = require('path');
+const electron = require('electron');
+
+const ROOT_DIR = path.dirname(__dirname);
 
 /* eslint-disable no-console */
-module.exports = function (gulp) { 'use strict';
-
-    const childProcess = require('child_process');
-    const electron = require('electron');
-
-    gulp.task('start', (next) => {
-        let child = childProcess.spawn(electron, ['./']);
-
-        child.stdout.on('data', (data) => {
-            console.log(data.toString('utf-8'));
-        });
-
-        child.on('exit', (code) => {
-            console.log('Child exited with code: ' + code);
-            return next(code === 1 ? new Error('Error running run task') : null);
-        });
+function startRenderer() {
+    const child = childProcess.spawn(electron, [ROOT_DIR, '--trace-warnings'], {
+        stdio: 'pipe'
     });
-};
+
+    child.on('exit', (code) => {
+        console.log('Child exited with code: ' + code);
+    });
+}
+
+module.exports = {startRenderer};
