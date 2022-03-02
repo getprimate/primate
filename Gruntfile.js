319ffd23ecb954ba2222513a5055c491f7d346b2
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-03-02 13:52:07 +0530

    add: Cleanup before compile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,23 @@
 'use strict';
 
+const loadGruntTasks = require('load-grunt-tasks');
+
+const {cleanBuild} = require('./tasks/clean');
 const {startRenderer} = require('./tasks/start');
 
+const copyConfig = require('./tasks/config/copy');
+const compilerConfig = require('./tasks/config/compiler');
+
 module.exports = function (grunt) {
+    loadGruntTasks(grunt, {pattern: ['grunt-contrib-copy', 'grunt-babel']});
+
+    grunt.initConfig({
+        copy: copyConfig,
+        babel: compilerConfig
+    });
+
+    grunt.registerTask('clean', 'Cleans up the build directory.', cleanBuild);
+
+    grunt.registerTask('compile', ['clean', 'babel', 'copy']);
     grunt.registerTask('start', 'Starts the application', startRenderer);
 };
