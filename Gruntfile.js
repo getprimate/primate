9a9e15a5e685b547144110b01ef534966afcfef0
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-03-03 10:05:25 +0530

    FIX: Build cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ const loadGruntTasks = require('load-grunt-tasks');
 
 const {cleanBuild} = require('./tasks/clean');
 const {startRenderer} = require('./tasks/start');
+const {makeRelease} = require('./tasks/release');
 
 const copyConfig = require('./tasks/config/copy');
 const compilerConfig = require('./tasks/config/compiler');
@@ -17,7 +18,9 @@ module.exports = function (grunt) {
     });
 
     grunt.registerTask('clean', 'Cleans up the build directory.', cleanBuild);
+    grunt.registerTask('pack', 'Packages the application.', makeRelease);
 
     grunt.registerTask('compile', ['clean', 'babel', 'copy']);
     grunt.registerTask('start', 'Starts the application', startRenderer);
+    grunt.registerTask('release', ['compile', 'pack']);
 };
