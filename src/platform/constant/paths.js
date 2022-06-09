ced44a62764c899f00eb14c2e52b687bb3b16c53
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-06-09 23:02:52 +0530

    add: Export RESOURCES_PATH constant

--- src/platform/constant/paths.js
+++ src/platform/constant/paths.js
@@ -7,7 +7,9 @@
 
 'use strict';
 
-const path = require('path');
+const path = require('node:path');
+
+const {app} = require('electron');
 
 const PLATFORM_PATH = path.dirname(__dirname);
 
@@ -15,4 +17,12 @@ const APP_PATH = path.dirname(PLATFORM_PATH);
 const ROOT_PATH = path.dirname(APP_PATH);
 const WORKBENCH_PATH = path.join(APP_PATH, 'workbench');
 
-module.exports = {ROOT_PATH, APP_PATH, PLATFORM_PATH, WORKBENCH_PATH};
+const RESOURCES_PATH = app.isPackaged ? ROOT_PATH : path.join(ROOT_PATH, 'resources');
+
+module.exports = {
+    ROOT_PATH,
+    APP_PATH,
+    PLATFORM_PATH,
+    WORKBENCH_PATH,
+    RESOURCES_PATH
+};
