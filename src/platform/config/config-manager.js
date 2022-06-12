6618b685dc40dd57713d3a83600401349c137512
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-06-12 16:18:31 +0530

    fix: Remove blind loading of configuration

--- src/platform/config/config-manager.js
+++ src/platform/config/config-manager.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Ajay Sreedhar. All rights reserved.
+ *
+ * Licensed under the MIT License.
+ * Please see LICENSE file located in the project root for more information.
+ */
+
 'use strict';
 
 const fs = require('node:fs');
