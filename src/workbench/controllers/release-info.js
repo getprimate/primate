4bd18a4a41c47faba0e13af2ff8c003887ee4bdc
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-06-21 23:37:31 +0530

    add: Placeholder for update info

--- src/workbench/controllers/release-info.js
+++ src/workbench/controllers/release-info.js
@@ -7,10 +7,12 @@
 
 'use strict';
 
+import {greaterThan} from '../lib/version-utils.js';
+import {epochToDate} from '../helpers/date-lib.js';
 import {FetchReleaseInfo} from '../helpers/release-repo.js';
 import releaseModel from '../models/release-model.js';
 
-const {document, appBridge} = window;
+const {appBridge} = window;
 
 /**
  * Provides a generic controller constructor for release information.
@@ -20,26 +22,40 @@ const {document, appBridge} = window;
  * @param {ViewFrameFactory} viewFrame - Factory for sharing UI details.
  */
 export default function ReleaseInfoController(scope, routeParams, restClient, viewFrame, toast) {
-    scope.appVersion = appBridge.getVersion();
+    scope.installedVersion = appBridge.getVersion();
+    scope.upgradedVersion = 'Checking...';
+
+    scope.releaseModel = releaseModel;
+    scope.releaseModel.publishedAt = epochToDate(releaseModel.publishedAt, viewFrame.getConfig('dateFormat'));
 
     const fetchReleaseInfo = FetchReleaseInfo.bind({_restClient: restClient});
     const request = fetchReleaseInfo(routeParams.version);
 
-    request.then((release) => {
-        scope.releaseModel = release;
+    request.then(({releaseIndex, releaseInfo}) => {
+        console.log(JSON.stringify(releaseIndex, null, 4), JSON.stringify(releaseInfo, null, 4));
+
+        if (Array.isArray(releaseIndex.contributors) && Array.isArray(releaseInfo.contributors)) {
+            releaseInfo.contributors = releaseIndex.contributors.concat(releaseInfo.contributors);
+        }
+
+        releaseInfo.publishedAt = epochToDate(releaseInfo.publishedAt, viewFrame.getConfig('dateFormat'));
+
+        scope.$apply((scope_) => {
+            scope_.upgradedVersion = releaseIndex.latest.stable;
+            scope_.releaseModel = releaseInfo;
+
+            if (greaterThan(releaseIndex.latest.stable, scope_.installedVersion)) {
+                scope_.upgradeLink = 'https://www.getprimate.xyz/download';
+            }
+        });
     });
 
-    request.catch((error) => {
-        toast.error('Unable to fetch release information.');
+    request.catch(() => {
+        if (routeParams.version === 'current') toast.error('Internet connection recommended.');
+        else toast.error('Unable to fetch release information.');
+
+        scope.upgradedVersion = 'Failed to check.';
     });
 
-    switch (routeParams.version) {
-        case 'current':
-            scope.releaseModel = releaseModel;
-            viewFrame.setTitle(`Primate v${scope.appVersion} - Release Notes`);
-            break;
-
-        default:
-            break;
-    }
+    viewFrame.setTitle(`Release Notes`);
 }
