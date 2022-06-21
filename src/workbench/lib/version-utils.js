eaa1cff0011beec883e5da8bd53b15e6df09527e
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-06-21 19:35:06 +0530

    add: Version utils library

---
+++ src/workbench/lib/version-utils.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) Ajay Sreedhar. All rights reserved.
+ *
+ * Licensed under the MIT License.
+ * Please see LICENSE file located in the project root for more information.
+ */
+
+'use strict';
+
+import {explode} from './core-toolkit.js';
+
+export function greaterThan(vLeft, vRight) {
+    if (vLeft === vRight) return false;
+
+    const lParts = explode(vLeft, '.').map((part) => parseInt(part));
+    const rParts = explode(vRight, '.').map((part) => parseInt(part));
+
+    for (let index in lParts) {
+        if (lParts[index] === rParts[index]) {
+            continue;
+        }
+
+        if (lParts[index] > rParts[index]) {
+            return true;
+        }
+
+        return false;
+    }
+}
