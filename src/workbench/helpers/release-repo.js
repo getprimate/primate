4379b89767ee3cb12d4dfdc1d7ca47a0d49939a6
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-06-21 12:49:01 +0530

    add: Add release repo helper functions

---
+++ src/workbench/helpers/release-repo.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) Ajay Sreedhar. All rights reserved.
+ *
+ * Licensed under the MIT License.
+ * Please see LICENSE file located in the project root for more information.
+ */
+
+'use strict';
+
+const repository = 'https://raw.githubusercontent.com/getprimate/release-info/main';
+
+/**
+ * Retrieves version ifo from the repository.
+ *
+ * This function should not be called directly.
+ * Allways create a bound function with the below context:
+ *
+ * @property {RESTClientFactory} _restClient - The rest client factory.
+ *
+ * @param {string} version - The version text.
+ * @returns {Object} Ther retrieved version information.
+ */
+export async function FetchReleaseInfo(version) {
+    const options = {method: 'GET', headers: {}, url: `${repository}/index.json`};
+
+    if (version === 'latest') {
+        const {data: index} = await this._restClient.request(options);
+        options.url = `${repository}/versions/v${index.latest.stable}.json`;
+        //
+    } else if (version === 'current') {
+        const current = appBridge.getVersion();
+        options.url = `${repository}/versions/v${current}.json`;
+        //
+    } else {
+        options.url = `${repository}/versions/v${version}.json`;
+    }
+
+    const {data: release} = await this._restClient.request(options);
+    return release;
+}
