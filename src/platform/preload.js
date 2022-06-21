27d5f83f8949d9fdeb245065eac756e9742d7eb9
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-06-21 12:50:07 +0530

    add: Expose appBridge in main world

--- src/platform/preload.js
+++ src/platform/preload.js
@@ -3,4 +3,11 @@
 const {contextBridge} = require('electron');
 const {ipcBridge} = require('./ipc/ipc-bridge');
 
+const appBridge = {
+    getVersion() {
+        return '1.0.0';
+    }
+};
+
+contextBridge.exposeInMainWorld('appBridge', appBridge);
 contextBridge.exposeInMainWorld('ipcBridge', ipcBridge);
