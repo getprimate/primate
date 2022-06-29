bed76f14af1b3b8783347d8fe826a34581edad34
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-06-29 15:04:17 +0530

    add: Product constants

---
+++ src/platform/constant/product.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) Ajay Sreedhar. All rights reserved.
+ *
+ * Licensed under the MIT License.
+ * Please see LICENSE file located in the project root for more information.
+ */
+
+'use strict';
+
+module.exports = {
+    APP_NAME: 'Primate'
+};
