8554e4e983ec4d389c25e7840f2479467debbd4e
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-06-29 21:18:37 +0530

    add: Release notes

--- src/workbench/models/release-model.js
+++ src/workbench/models/release-model.js
@@ -16,30 +16,44 @@ export default {
     releaseNotes: {
         features: [
             {
-                primaryText: 'Stores multiple connections in history.',
+                primaryText: 'KongDash is now renamed as Primate.',
                 description: []
             },
-            {
-                primaryText: 'Supports UI customizations with themes.',
-                description: ['Themes can be placed in <app-data>/primate-ce/User-Themes</i> directory.']
-            }
-        ],
-        bugfixes: [
-            {
-                primaryText: 'Fixed host configuration issue in settings view.',
-                description: []
-            }
-        ],
-        upgrades: [
             {
                 primaryText: 'Supports Kong Admin API v2.7.x',
                 description: []
             },
+            {
+                primaryText: 'Stores multiple connections in history.',
+                description: ['All connection information are stored locally in the app data directory.']
+            },
+            {
+                primaryText: 'Supports UI customizations with themes.',
+                description: ['Themes can be placed in <app-data>/primate-ce/User-Themes</i> directory.']
+            },
+            {
+                primaryText: 'Added an activity monitor that logs HTTP API calls.',
+                description: ['Monitor is helpful while checking HTTP request status.']
+            }
+
+        ],
+        bugfixes: [
+            {
+                primaryText: 'Fixed a host configuration issue in settings view.',
+                description: []
+            }
+        ],
+        upgrades: [
             {
                 primaryText: 'Electron to version 19.x.x',
                 description: []
             }
         ]
     },
-    contributors: []
+    contributors: [{
+        username: 'ajaysreedhar',
+        fullname: 'Ajay Sreedhar',
+        profileLink: 'https://github.com/ajaysreedhar',
+        description: 'Project maintainer'
+    }]
 };
