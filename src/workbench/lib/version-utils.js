99b3741df1a04fda9e9ef24e38ca33af21cedd8b
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-07-15 20:17:56 +0530

    add: New genericForm function

--- src/workbench/lib/version-utils.js
+++ src/workbench/lib/version-utils.js
@@ -32,3 +32,39 @@ export function greaterThan(vLeft, vRight) {
         return false;
     }
 }
+
+/**
+ * Returns the generic semantic version form.
+ *
+ * For example:
+ * - Input: 2.7.0
+ *   Output: 2.7.z (places = 1)
+ * - Input: 2.7.0
+ *   Output: 2.y.z (places = 2)
+ *
+ * @param {string} version - The version string.
+ * @param {number} places - The place value.
+ * @return {string} The semantic version form.
+ */
+export function genericForm(version, places = 1) {
+    const parts = version.split('.').reduce((list, value, index) => {
+        const part = parseInt(value);
+
+        if (false === isNaN(part) && index <= 2) {
+            list.push(part);
+        }
+
+        return list;
+    }, []);
+    const chars = ['x', 'y', 'z'];
+
+    for (let index = parts.length - 1; index >= 0; index--) {
+        if (places > 0) {
+            parts[index] = chars[index];
+        }
+
+        places--;
+    }
+
+    return parts.join('.');
+}
