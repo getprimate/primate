a05b90686b1a22f37e5d9f6ef4972220346dc45e
ajaysreedhar <sree.ajay468@gmail.com>
2024-02-07 21:44:53 +0530

    add: Copy about dialog boilerplate

---
+++ src/platform/ui/about-dialog.ts
@@ -0,0 +1,44 @@
+/**
+ * ------------------------------------------------------------
+ * 
+ * Copyright (c) 2022-present Ajay Sreedhar.
+ *
+ * Licensed under the MIT License.
+ * See LICENSE file located in the root directory.
+ * 
+ * ============================================================
+ */
+
+import os from "node:os";
+import {app, dialog, MessageBoxOptions} from "electron";
+
+const extended: Record<string, string> = {
+    Version: app.getVersion(),
+    Electron: process.versions.electron,
+    Date: "September 10, 2023",
+    Platform: os.release()
+};
+
+function buildDetails(details: Record<string, string>): string {
+    const keys: string[] = Object.keys(details);
+    const list: string[] = [];
+
+    for (const key of keys) {
+        list.push(`${key}: ${details[key]}`);
+    }
+
+    return list.join("\n");
+}
+
+const options: MessageBoxOptions = {
+    title: "Primate",
+    message: "About Primate",
+    type: "info",
+    detail: buildDetails(extended)
+};
+
+function showAboutDialog(window): void {
+    dialog.showMessageBox(window, options).finally(() => { /* Do notihing. */ });
+}
+
+export default {showAboutDialog};
