44da514a0d40dccbb9fcdc0540c0ceeda1af48bf
ajaysreedhar <sree.ajay468@gmail.com>
2026-09-23 12:34:38 +0530

    docs: cleanup file header block comment

--- src/workbench/controllers/consumer-list.js
+++ src/workbench/controllers/consumer-list.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) Ajay Sreedhar. All rights reserved.
- *
- * Licensed under the MIT License.
- * Please see LICENSE file located in the project root for more information.
- */
-
 'use strict';
 
 import {isNil, isText} from '../lib/core-toolkit.js';
